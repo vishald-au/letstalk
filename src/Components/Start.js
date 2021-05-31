5c459b552777b9b4877766ed4223cc523bd28db6
Vishal Dhingra <55521133+vishald-au@users.noreply.github.com>
2021-06-01 00:08:06 +0930

    in progress

---
+++ src/Components/Start.js
@@ -0,0 +1,12 @@
+
+
+const Start = () => {
+    return (
+        <div className='StartHere'>
+            <div className='lightArea'></div>
+            <div className='darkArea'></div>
+        </div>
+    )
+}
+
+export default Start
